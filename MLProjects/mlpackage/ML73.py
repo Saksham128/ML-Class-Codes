1fc35596b4f0091b683e958a0850d0de24577716
Saksham Badoni <sbadoni.cs@doonuniversity.ac.in>
2022-01-29 19:12:48 +0530

    Day 21 codes added

---
+++ MLProjects/mlpackage/ML73.py
@@ -0,0 +1,21 @@
+# precision(= exactness / quality)
+#           = how useful the search results are
+#           = measure of exactness or quality
+
+# recall ( = completeness / quantity / sensitivity)
+#          = how complete the results are
+#          = measure of completeness or quantity.
+
+#F1 score = is the harmonic mean of precision and sensitivity(=recall)
+# F1 = 2 x ( (precission x recall) / (precission + recall) )
+#Question-1:
+#------------------
+# precission=.81
+# recall = .88
+# F1 = ?          #0.84
+
+#Question-2:
+#------------------
+# precission =.74
+# recall =   .63
+# F1 = ?         #0.68
